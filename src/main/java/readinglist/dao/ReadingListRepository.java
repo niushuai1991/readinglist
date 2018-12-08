22f9ae86256fdd98a1210d72019e5a88047e93a5
niushuai <guisu2010@gmail.com>
2018-12-08 23:52:35 +0800

    完成第2章

---
+++ src/main/java/readinglist/dao/ReadingListRepository.java
@@ -0,0 +1,13 @@
+package readinglist.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import readinglist.entity.Book;
+
+import java.util.List;
+
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByReader(String reader);
+
+
+}
